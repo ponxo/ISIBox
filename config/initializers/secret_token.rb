82669b171d19c3acf1729005e11beb03d4c5174e
Alvaro Alonso Carriazo <alvaroalonso1989@gmail.com>
2013-01-15 17:33:21 +0100

    Comienzo del proyecto ISIBox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ISIBox::Application.config.secret_token = '56f7f73d39bd5b2b9a69a43564162ca05748a5262573a67261ec4b94079b316bfd878871bf1acdebe346f87b27bc9863b05aff8c29db39467ef0d54f255d2bb6'
