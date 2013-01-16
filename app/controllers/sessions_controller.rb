64b79f1c45e6509f53d577f83202981ead697034
Alvaro Alonso Carriazo <alvaroalonso1989@gmail.com>
2013-01-16 20:10:16 +0100

    Controller session, funcion login creada

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
 
+  def login
+    @nick=params[:usuario][:Nick]
+    @password=params[:usuario][:password]
+    @user=Usuario.find_by_nick(@nick)
+    if @user then
+      if @user.password == @password then
+        session[:usuario]=@user
+        redirect _to isibox_index_path
+      end
+      else
+        flash[:warning]="Fallo de autenticacion"
+        redirect_to homepage_path
+      end
+    end
+    else
+      flash[:warning]="Fallo de autenticacion"
+      redirect_to homepage_path
+    end
+  end
 
 end
