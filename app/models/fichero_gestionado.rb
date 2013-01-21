4875f8afbf70213b91fb3e3bf6244abb3c3f8afd
Alvaro Alonso Carriazo <alvaroalonso1989@gmail.com>
2013-01-21 19:05:07 +0100

    Añadido campo file al modelo, y modificado fallo al añadir la s en belongs

--- app/models/fichero_gestionado.rb
+++ app/models/fichero_gestionado.rb
@@ -1,4 +1,5 @@
 class FicheroGestionado < ActiveRecord::Base
-  attr_accessible :content_type, :data, :filename
-  belong_to :usuario
+  attr_accessible :content_type, :path, :filename, :file
+  belongs_to :usuario
+  mount_uploader :file, ImageUploader
 end
