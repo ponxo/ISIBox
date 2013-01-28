014edc1e733eeaf98f117175d8fd5da3d98878a3
Alvaro Alonso Carriazo <alvaroalonso1989@gmail.com>
2013-01-28 12:18:18 +0100

    Dependent => destroy creado para la relacion entre usuarios y ficheros

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,5 @@
 class Usuario < ActiveRecord::Base
   attr_accessible :nick, :password
   validates_uniqueness_of :nick
-  has_many :fichero_gestionados
+  has_many :fichero_gestionados, :dependent => :destroy
 end
